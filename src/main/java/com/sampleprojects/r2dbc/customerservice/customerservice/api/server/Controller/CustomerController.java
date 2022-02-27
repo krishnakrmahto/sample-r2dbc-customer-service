c5f2e60f7556460a51fd8e69159e14cf1e63ee0c
krishnakrmahto97 <krish.thorcode@gmail.com>
2022-02-27 22:41:17 +0530

    Get controller endpoint MediaType to event stream for server sent events over http in the form of Publisher/Flux

--- src/main/java/com/sampleprojects/r2dbc/customerservice/customerservice/api/server/Controller/CustomerController.java
+++ src/main/java/com/sampleprojects/r2dbc/customerservice/customerservice/api/server/Controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.sampleprojects.r2dbc.customerservice.customerservice.entity.Customer;
 import com.sampleprojects.r2dbc.customerservice.customerservice.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.reactivestreams.Publisher;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class CustomerController {
 
   private final CustomerService service;
 
-  @GetMapping
+  @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
   public Publisher<Customer> getAllCustomers() {
     return service.getAllCustomers();
   }
--- src/main/java/com/sampleprojects/r2dbc/customerservice/customerservice/service/CustomerService.java
+++ src/main/java/com/sampleprojects/r2dbc/customerservice/customerservice/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.sampleprojects.r2dbc.customerservice.customerservice.service;
 import com.sampleprojects.r2dbc.customerservice.customerservice.entity.Customer;
 import com.sampleprojects.r2dbc.customerservice.customerservice.repository.CustomerRepository;
 import com.sampleprojects.r2dbc.customerservice.customerservice.util.demo.DatabaseSampleInitializer;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,8 @@ public class CustomerService {
 
   public Publisher<Customer> getAllCustomers() {
     return dataInitializer.initializeDatabaseWithSampleValues()
-        .thenMany(repository.findAll());
+        .thenMany(repository.findAll())
+        .delayElements(Duration.ofSeconds(2));
   }
 
 }
